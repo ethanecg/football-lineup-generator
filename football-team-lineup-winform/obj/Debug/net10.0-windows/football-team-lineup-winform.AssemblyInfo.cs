e8688df62e03b4ad4766f8574105b23a6e3010e7
ethanecg <2558030@cegepmontpetit.ca>
2026-07-06 14:15:56 -0400

    Delete some csv to make it clean.

--- football-team-lineup-winform/obj/Debug/net10.0-windows/football-team-lineup-winform.AssemblyInfo.cs
+++ football-team-lineup-winform/obj/Debug/net10.0-windows/football-team-lineup-winform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("football-team-lineup-winform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ada96387bce99c6b9980e0c06a7dd46445e602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138f6c8b0d8abb93871aa283f6c2dce93a6fda85")]
 [assembly: System.Reflection.AssemblyProductAttribute("football-team-lineup-winform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("football-team-lineup-winform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
